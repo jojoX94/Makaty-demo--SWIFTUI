22da504f9492798a1e06155c71ec0c0706cf0fa2
jojoX94 <ramarolacontact@gmail.com>
2022-12-01 15:02:05 +0300

    Init project

---
+++ Makaty_UI/ContentView.swift
@@ -0,0 +1,23 @@
+
+//  ContentView.swift
+//  Makaty_UI
+//
+//  Created by Madiapps on 01/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, everyone!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Makaty_UI/Makaty_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Makaty_UIApp.swift
+//  Makaty_UI
+//
+//  Created by Madiapps on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Makaty_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
