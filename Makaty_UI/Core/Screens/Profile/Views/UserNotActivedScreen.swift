3d8ceb0b5490cff18868a3c9a906ce2cd0e71295
jojoX94 <ramarolacontact@gmail.com>
2022-12-26 09:56:24 +0300

    UserNotActivedScreen created

---
+++ Makaty_UI/Core/Screens/Profile/Views/UserNotActivedScreen.swift
@@ -0,0 +1,64 @@
+//
+//  UserNotActivedScreen.swift
+//  Makaty_UI
+//
+//  Created by Madiapps on 26/12/2022.
+//
+
+import SwiftUI
+
+struct UserNotActivedScreen: View {
+    
+    @EnvironmentObject var viewRouter : ViewRoutterViewModel
+    
+    @State var receiveNotif: Bool = true
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 0) {
+                VStack(spacing: 32) {
+                    Image("NoprofilImg")
+                    VStack(spacing: 16) {
+                        Text("Compte visiteur")
+                            .textSubtitleStyle()
+                        Text("Morbi euismod nec dolor elementum quis. Integer sit donec eget sed nam nullam. Diam ac turpis diam tempus blandit mi.")
+                            .font(.custom("SFProText-Regular", size: 16))
+                            .foregroundColor(Color("Gray"))
+                            .multilineTextAlignment(.center)
+                    }
+                }
+                .padding(.horizontal, 40)
+                
+                Spacer()
+                
+                Link(destination: URL(string: "http://www.makaty.mg/")!) {
+                    LargeButton(title: "Visitez le site", subTitle: "makaty.mg", backgroundImageName: "PointsImg_clean")
+                }
+                .padding(.horizontal, 16)
+                
+                
+                
+                
+            }
+            .padding(.top, 40)
+            .padding(.bottom, 20)
+            .background(Color.white)
+            .toolbar {
+                CustomToolBarContent(titleType: .text, title: "Mon profil") {
+                    withAnimation {
+                        viewRouter.showMenu = true
+                    }
+                }
+            }
+            
+        }
+        
+    }
+}
+
+struct UserNotActivedScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        UserNotActivedScreen()
+            .environmentObject(ViewRoutterViewModel())
+    }
+}
